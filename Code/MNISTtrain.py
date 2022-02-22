1ffccc2a64f044b2d75b6da00d2bf9aa9507d7a9
FrancescaDavidde <francesca.davidde@gmail.com>
2022-02-22 19:31:35 +0100

    upload

---
+++ Code/MNISTtrain.py
@@ -0,0 +1,248 @@
+import tensorflow as tf
+import binNN.networks as networks
+import math
+import os
+import datetime
+now = datetime.datetime.now()
+from tqdm import tqdm
+import binNN.optimizers as optimizers
+import binNN.input_data as input_data
+import matplotlib.pyplot as plt
+
+'''
+
+MNIST TRAINING
+
+'''
+
+def load_mnist():
+    mnist = input_data.read_data_sets('dataset/MNIST_data', one_hot=False)
+    x_train = mnist.train.images
+    y_train = mnist.train.labels
+    x_test = mnist.test.images
+    y_test = mnist.test.labels
+
+    #      x_train, y_train, x_test, y_test, num_classes
+    return x_train, y_train, x_test, y_test, 10
+
+''' ---------------- PARAMETERS OF THE NETWORK ----------------------'''
+# Type of network to be used: 2 choices
+# NETWORK = 'binary'
+NETWORK = 'binary_sbn'
+
+# Dataset to be used for the learning task
+DATASET = 'mnist'
+# path where to save networks weights
+MODELDIR = './models/'
+# folder for tensorboard logs
+LOGDIR = './logs/'
+
+# Number of epochs performed during training
+EPOCHS = 20
+# Dimension of the training batch
+BATCH_SIZE = 100
+# Starting optimizer learning rate value
+STEPSIZE = 1e-3
+
+# Toggle th use of shift based AdaMax instead of vanilla Adam optimizer
+SHIFT_OPT = True  # if true use AdaMax
+''' ---------------------------------------------------------------'''
+
+# creating directory and files for saving model
+timestamp = now.strftime("%m-%d %H:%M")
+time = now.strftime("%H-%M")
+date = datetime.date.today()
+model_name = ''.join([str(date), '_', NETWORK, '_', DATASET, '_', str(time)])
+session_logdir = os.path.join(LOGDIR, model_name)
+train_logdir = os.path.join(session_logdir, 'train')
+test_logdir = os.path.join(session_logdir, 'test')
+session_modeldir = os.path.join(MODELDIR, model_name)
+
+if not os.path.exists(session_modeldir):
+    os.makedirs(session_modeldir)
+if not os.path.exists(train_logdir):
+    os.makedirs(train_logdir)
+if not os.path.exists(test_logdir):
+    os.makedirs(test_logdir)
+
+# dataset preparation using tensorflow dataset iterators
+x_train, y_train, x_test, y_test, num_classes = load_mnist()
+
+batch_size = tf.placeholder(tf.int64)
+data_features, data_labels = tf.placeholder(tf.float32, (None,) + x_train.shape[1:]), tf.placeholder(tf.int32, (
+None,) + y_train.shape[1:])
+
+# preparing training data from dataset
+train_data = tf.data.Dataset.from_tensor_slices((data_features, data_labels))
+train_data = train_data.repeat().shuffle(x_train.shape[0]).batch(batch_size)
+# preparing test data from dataset
+test_data = tf.data.Dataset.from_tensor_slices((data_features, data_labels))
+test_data = test_data.repeat().shuffle(x_test.shape[0]).batch(batch_size)
+
+# create an object iterator over selected data structure
+data_iterator = tf.data.Iterator.from_structure(train_data.output_types, train_data.output_shapes)
+
+# function to get next features and labels from dataset
+features, labels = data_iterator.get_next()
+train_initialization = data_iterator.make_initializer(train_data)
+
+test_initialization = data_iterator.make_initializer(test_data)
+
+# network initialization at training state = TRUE
+is_training = tf.get_variable('is_training', initializer=tf.constant(True, tf.bool))
+# function to switch training state to false = test
+switch_training_inference = tf.assign(is_training, tf.logical_not(is_training))
+
+# HERE WE CREATE THE NETWORK ACCORDING TO INPUTS
+xnet, ynet = networks.get_network(NETWORK, DATASET, features, training=is_training)
+
+
+with tf.name_scope('trainer_optimizer'):
+
+    learning_rate = tf.Variable(STEPSIZE, name='learning_rate')
+    learning_rate_decay = tf.placeholder(tf.float32, shape=(), name='lr_decay')
+    update_learning_rate = tf.assign(learning_rate, learning_rate * learning_rate_decay)
+
+    # optimizer costructor
+    opt_constructor = optimizers.ShiftBasedAdaMaxOptimizer if SHIFT_OPT else tf.train.AdamOptimizer
+    optimizer = opt_constructor(learning_rate=learning_rate)
+    # computes softmax cross entropy between labels and logits
+    # we use sparse cause our dataset is not ONE HOT labelled
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=ynet, labels=labels)
+    # compute accuracy of classification
+    loss = tf.reduce_mean(cross_entropy)
+
+
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        global_step = tf.train.get_or_create_global_step()
+        train_op = optimizer.minimize(loss=loss, global_step=global_step)
+
+# metrics definition
+with tf.variable_scope('metrics'):
+    mloss, mloss_update = tf.metrics.mean(cross_entropy)
+
+    # tf.metrics.accuracy calculates how often predictions matches labels based on two local variables
+    accuracy, acc_update = tf.metrics.accuracy(labels, tf.argmax(ynet, axis=1))
+
+    metrics = [mloss, accuracy]
+    metrics_update = [mloss_update, acc_update]
+
+# Isolate the variables stored behind the scenes by the metric operation
+metrics_variables = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope="metrics")
+metrics_initializer = tf.variables_initializer(metrics_variables)
+
+# summaries
+los_sum = tf.summary.scalar('loss', mloss)
+acc_sum = tf.summary.scalar('accuracy', accuracy)
+merged_summary = tf.summary.merge([los_sum, acc_sum])
+
+# network weights saver
+saver = tf.train.Saver()
+
+NUM_BATCHES_TRAIN = math.ceil(x_train.shape[0] / BATCH_SIZE)
+NUM_BATCHES_TEST = math.ceil(x_test.shape[0] / BATCH_SIZE)
+
+# Plot settings
+
+set_training_loss = []
+set_training_acc = []
+epoch_set = []
+
+# now we start the session
+with tf.Session() as sess:
+    print(now.strftime("%Y-%m-%d %H:%M"))
+
+    # tensorboard summary writer
+    train_writer = tf.summary.FileWriter(train_logdir, sess.graph)
+    test_writer = tf.summary.FileWriter(test_logdir)
+
+    sess.run(tf.global_variables_initializer())
+
+    for epoch in range(EPOCHS):
+
+        print("\nEPOCH %d/%d" % (epoch + 1, EPOCHS))
+
+        # exponential learning rate decay update
+        sess.run(update_learning_rate, feed_dict={learning_rate_decay: math.pow(0.91, epoch)})
+
+        # initialize training and set batch normalization training
+        sess.run(train_initialization, feed_dict={data_features: x_train, data_labels: y_train, batch_size: BATCH_SIZE})
+
+        # metrics are used to computed all infos needed
+        sess.run(metrics_initializer)
+        # sess.run(switch_training_inference)
+
+        print("is training? ", sess.run(is_training))
+
+        # Training of the network
+        for current_batch in tqdm(range(NUM_BATCHES_TRAIN)):
+            sess.run(train_op)  # train network on a single batch
+            batch_trn_loss, _ = sess.run(metrics_update)
+            # here we get loss and accuracy via metrics measures
+            training_loss, training_accuracy = sess.run(metrics)
+
+        print("")
+        print("Training loss: ", round(training_loss, 3), " Training accuracy: ", round(training_accuracy, 3))
+        print("------------------------------------------------------------")
+
+        # add to models
+        summary = sess.run(merged_summary)
+        train_writer.add_summary(summary, epoch)
+
+        if EPOCHS-epoch-1 == 0:
+            # test is at last epoch
+            print("test STARTS")
+            # initialize the test dataset and set batc normalization inference
+            sess.run(test_initialization, feed_dict={data_features: x_test, data_labels: y_test, batch_size: BATCH_SIZE})
+            sess.run(metrics_initializer)
+
+            sess.run(switch_training_inference) # changes is_training = false
+            print("is training? ",sess.run(is_training))
+
+            # evaluation of the network
+            for current_batch in tqdm(range(NUM_BATCHES_TEST)):
+                sess.run([loss, metrics_update])
+                # compute loss and test accuracy
+                test_loss, test_accuracy = sess.run(metrics)
+
+            print("")
+            print("Test loss: ", round(test_loss, 3), " Test accuracy: ", round(test_accuracy, 3))
+            print("------------------------------------------------------------")
+
+            summary = sess.run(merged_summary)
+            test_writer.add_summary(summary, epoch)
+
+        # -------  inserisco nel grafico  ----------------
+        epoch_set.append(epoch + 1)
+        set_training_loss.append(training_loss)
+        set_training_acc.append(training_accuracy)
+        # -------------------------------------------------
+
+    train_writer.close()
+    test_writer.close()
+
+    # save model at the end of operations
+    saver.save(sess, os.path.join(session_modeldir, 'model.ckpt'))
+
+    plt.figure(1)
+    # figure 1.1 is about loss variance in time for training and test
+    plt.subplot(211)
+    plt.plot(epoch_set, set_training_loss, 'b', label='training')
+    plt.legend()
+    plt.xlabel('epochs')
+    plt.ylabel('LOSS')
+
+    # figure 1.2 is about accuracy variance in time for training and test
+    plt.subplot(212)
+    plt.plot(epoch_set, set_training_acc, 'b', label='training')
+    plt.legend()
+    plt.xlabel('epochs')
+    plt.ylabel('ACCURACY')
+
+    plt.show()
+    end = now.strftime("%m-%d %H:%M")
+
+print('\nTraining completed!\nNetwork model is saved in  {}\nTraining logs are saved in {}'.format(session_modeldir,session_logdir))
+print("Ended :", end)
+print('\nOpen TensorBoard by tiping :\ntensorboard --logdir=logs/')
---
+++ Code/layers.py
@@ -0,0 +1,198 @@
+import tensorflow as tf
+from tensorflow.python.framework import ops
+
+#metodi utilizzati dentro network (per creare le reti)
+
+
+def binarize(x):
+	# we also have to reassign the sign gradient otherwise it will be almost everywhere equal to zero
+	# using the straight through estimator
+	with tf.get_default_graph().gradient_override_map({'Sign': 'Identity'}):
+		return tf.sign(tf.sign(x)+1e-8)
+
+
+# dense per le reti binarie
+# NB sono utilizzati infatti binarize e clip_by_value
+	
+def binaryDense(inputs, units, activation=None, use_bias=False, trainable=True, binarize_input=True, name='binarydense', reuse=False):
+	
+	# flatten the input 
+	flat_input = tf.contrib.layers.flatten(inputs)
+	
+	# count all the input units (thus check the shape without considering the batch size)
+	in_units = flat_input.get_shape().as_list()[1]
+	
+	with tf.variable_scope(name, reuse=reuse):
+		# getting layer weights and add clip operation (between -1, 1)
+		
+		# xavier initializer : This initializer is designed to keep the scale of the gradients roughly the same in all layers
+		xavier = tf.contrib.layers.xavier_initializer()
+		random = tf.initializers.random_uniform()
+
+		w = tf.get_variable('weight', [in_units, units], initializer=xavier, trainable=trainable)
+		w = tf.clip_by_value(w, -1, 1)
+		
+		# binarize input and weights of the layer
+		if binarize_input:
+			flat_input = binarize(flat_input)
+		w = binarize(w)
+		
+		# adding layer operation -> (w*x + b)
+		out = tf.matmul(flat_input, w)
+		
+		''' not used
+		if use_bias:
+			b = tf.get_variable('bias', [units], initializer=tf.zeros_initializer(), trainable=trainable)
+			out = tf.nn.bias_add(out, b)à
+		'''
+		
+		# applying activation function
+		if activation:
+			out = activation(out)
+		
+		# activations collection is not automatically populated
+		tf.add_to_collection(tf.GraphKeys.ACTIVATIONS, out)
+		return out
+
+
+# conv2d per le reti binarie
+# NB sono utilizzati infatti binarize e clip_by_value
+
+def binaryConv2d(inputs, filters, kernel_size, strides, padding="VALID", use_bias=False, activation=None, binarize_input=True, trainable=True, 
+					reuse=False, use_cudnn_on_gpu=True, data_format='NHWC', dilations=[1,1,1,1], name='binaryconv2d'):
+	
+	assert len(strides) == 2
+	assert data_format in ['NHWC', 'NCHW']
+	
+	if data_format == 'NHWC':
+		strides = [1] + strides + [1]
+		in_ch = inputs.get_shape().as_list()[3]
+		wshape = kernel_size + [in_ch, filters]
+	elif data_format == 'NCHW':
+		strides = [1, 1] + strides
+		in_ch = inputs.get_shape().as_list()[1]	
+		wshape = [in_ch, filters] + kernel_size
+	
+	
+	with tf.variable_scope(name, reuse=reuse):
+		# getting filter weights and add clip operation on them (between -1, 1)			
+		fw = tf.get_variable('weight', wshape, trainable=trainable, initializer=tf.contrib.layers.xavier_initializer())
+		fw = tf.clip_by_value(fw, -1, 1)
+		
+		# binarize  input and weights of the layer
+		if binarize_input:
+			inputs = binarize(inputs)
+		fw = binarize(fw)
+		
+		# adding convolution
+		out = tf.nn.conv2d(inputs, fw, strides, padding, use_cudnn_on_gpu=use_cudnn_on_gpu, 
+							data_format=data_format, dilations=dilations)
+		
+		# adding bias
+		if use_bias:
+			fb = tf.get_variable('bias', [filters], initializer=tf.zeros_initializer, trainable=trainable)
+			out = tf.nn.bias_add(out, fb, data_format=data_format)
+		
+		# applying activation function
+		if activation:
+			out = activation(out, 'activation')
+		
+		# activation collection is not automatically populated
+		tf.add_to_collection(tf.GraphKeys.ACTIVATIONS, out)
+		
+		return out
+
+# ap2(x) viene calcolato come somma fra gradino(x) + 2^(round( log2 (modulo di x) )
+# compute the approximate power of 2 of the input x
+# via hardware it is as simple as get the index of the most significative bit
+def ap2(x):
+	return tf.sign(x) * tf.pow(2.0, tf.round(tf.log(tf.abs(x)) / tf.log(2.0)))
+
+'''
+In this work multiplication is still used somewhere cause we decide to not implement 
+'''
+
+# FOR DENSENET
+# Shift based Batch Normalizing Transform, applied to activation (x) over a mini-batch
+def shift_batch_norm(x, training=True, momentum=0.99, epsilon=1e-8, reuse=False, name="batch_norm"):
+	xshape = x.get_shape()[1:]
+
+	with tf.variable_scope(name, reuse=reuse):
+		# trainable parameters
+		gamma = tf.get_variable('gamma', xshape, initializer=tf.ones_initializer, trainable=True)
+		beta = tf.get_variable('beta', xshape, initializer=tf.zeros_initializer, trainable=True)
+		
+		mov_avg = tf.get_variable('mov_avg', xshape, initializer=tf.zeros_initializer, trainable=False)
+		mov_var = tf.get_variable('mov_std', xshape, initializer=tf.ones_initializer, trainable=False)
+		
+		def training_xdot():
+			avg = tf.reduce_mean(x, axis=0)							# feature means
+			cx = x - avg											# centered input
+			var = tf.reduce_mean(tf.multiply(cx, ap2(cx)), axis=0)  # apx variance
+			
+			# updating ops. for moving average and moving variance used at inference time
+			avg_update = tf.assign(mov_avg, momentum * mov_avg + (1.0 - momentum) * avg)
+			var_update = tf.assign(mov_var, momentum * mov_var + (1.0 - momentum) * var)
+			
+			with tf.control_dependencies([avg_update, var_update]):
+				return cx / ap2(tf.sqrt(var + epsilon))				# normalized input
+			
+		def inference_xdot():
+			return (x - mov_avg) / ap2(tf.sqrt(mov_var + epsilon))
+
+		# if training is true returns training_xdot else inference_xdot
+		xdot = tf.cond(training, training_xdot, inference_xdot)
+
+		# scale and shift input distribution
+		out = tf.multiply(ap2(gamma), xdot) + beta
+
+		# this is why shift is not implemented - int32 and float32 type error
+		# x = tf.cast(ap2(gamma), tf.int32)
+		# y = tf.cast(xdot, tf.int32)
+		# out = tf.compat.v2.bitwise.right_shift(x,y) + beta
+	
+	return out
+
+# Spatial shift based batch normalization, like spatial batch normalization it keeps
+# the convolution property. Hence it applies the same transformation to each element
+# of the same feature map
+
+def spatial_shift_batch_norm(x, data_format='NHWC', training=True, momentum=0.99, epsilon=1e-8, reuse=False, name="spatial_batch_norm"):
+	assert data_format in ['NHWC', 'NCHW']
+	
+	if data_format == "NHWC":
+		mean_axis = (0,1,2)
+		channel_axis = 3
+		ch_tensor_shape = [1, 1, 1, x.get_shape().as_list()[channel_axis]]
+	elif data_format == "NCHW":
+		mean_axis = (0,2,3)
+		channel_axis = 1
+		ch_tensor_shape = [1, x.get_shape().as_list()[channel_axis], 1, 1]
+	
+	with tf.variable_scope(name, reuse=reuse):
+		gamma = tf.get_variable('gamma', ch_tensor_shape, initializer=tf.ones_initializer, trainable=True)
+		beta  = tf.get_variable('beta', ch_tensor_shape, initializer=tf.zeros_initializer, trainable=True)
+		
+		mov_avg = tf.get_variable('mov_avg', ch_tensor_shape, initializer=tf.zeros_initializer, trainable=False)
+		mov_var = tf.get_variable('mov_std', ch_tensor_shape, initializer=tf.ones_initializer, trainable=False)
+		
+		def training_xdot():
+			avg = tf.reduce_mean(x, axis=mean_axis, keepdims=True)
+			cx = x - avg																	# centered input
+			var = tf.reduce_mean(tf.multiply(cx, ap2(cx)), axis=mean_axis, keepdims=True)	# apx variance
+			
+			# updating ops. for moving average and moving variance used at inference time
+			avg_update = tf.assign(mov_avg, momentum * mov_avg + (1.0 - momentum) * avg)
+			var_update = tf.assign(mov_var, momentum * mov_var + (1.0 - momentum) * var)
+			
+			with tf.control_dependencies([avg_update, var_update]):
+				return cx / ap2(tf.sqrt(var + epsilon))					# normalized input
+		
+		def inference_xdot():
+			return (x - mov_avg) / ap2(tf.sqrt(mov_var + epsilon))
+		
+		xdot = tf.cond(training, training_xdot, inference_xdot)
+		out = tf.multiply(ap2(gamma), xdot) + beta					# scale and shift input distribution
+	
+	return out
+	
